fa6a5eeb38a8ec75cbf02d0b96e7f9b666a6b35f
Pavel Asafov <pavelasaf@yandex.ru>
2022-12-14 23:36:34 +0600

    Сделано открытие окон

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,70 @@
 // Вызов элементов ------------------------------
 
-let popup = document.querySelector('.popup');
-let profile = document.querySelector('.profile');
-let form = document.querySelector('.form');
-// Открытие/закрытие окна
-let editProfile = profile.querySelector('.profile__edit');
-let popupClose = popup.querySelector('.popup__close');
-// Данные профиля на странице
-let nameProfile = profile.querySelector('.profile__name');
-let jobProfile = profile.querySelector('.profile__activity');
-// Данные профиля в попапе
-let nameInput = form.querySelector('.form__item_type_name');
-let jobInput = form.querySelector('.form__item_type_activity');
+const profile = document.querySelector('.profile');
+const editProfile = profile.querySelector('.profile__edit');
+const nameProfile = profile.querySelector('.profile__name');
+const jobProfile = profile.querySelector('.profile__activity');
+const addCard = profile.querySelector('.profile__add');
+
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close');
+
+const form = document.querySelector('.form');
+const nameInput = form.querySelector('.form__item_type_name');
+const jobInput = form.querySelector('.form__item_type_activity');
+
+const popupEditProfile = document.querySelector('.popup_type_profile');
+const popupAddCard = document.querySelector('.popup_type_card');
+
 
 // Функции --------------------------------------
 
-// Открытие окна
-function popupActive() {
+
+
+
+// Открытие диалогового окна
+function openPopup(popup) {
   popup.classList.add('popup_opened');
-  nameInput.value = nameProfile.textContent;
-  jobInput.value = jobProfile.textContent;
 }
-// Заккрытие окна
-function popupRemove() {
+
+// Заккрытие диалогового окна
+function removePopup(popup) {
   popup.classList.remove('popup_opened');
 }
+
 // Обработчик формы
 function handleFormSubmit (evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  popupRemove();
+  removePopup(popupEditProfile);
 }
 
 // Слушатели ------------------------------------
 
-editProfile.addEventListener('click', popupActive);
-popupClose.addEventListener('click', popupRemove);
-form.addEventListener('submit', handleFormSubmit); 
+// Button open edit profile
+editProfile.addEventListener('click', function() {
+  openPopup(popupEditProfile);
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+
+});
+
+// Button open add card
+addCard.addEventListener('click', function() {
+  openPopup(popupAddCard);
+});
+
+
+popupClose.addEventListener('click', function() {
+  removePopup(popupEditProfile);
+});
+
+popupClose.addEventListener('click', function() {
+  console.log(removePopup(popupAddCard));
+});
+
+
+
+
+form.addEventListener('submit', handleFormSubmit);
