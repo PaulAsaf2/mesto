02ae83a519733e7252de5a785bb85b5c6dc0f9d7
Pavel Asafov <pavelasaf@yandex.ru>
2023-01-24 16:17:13 +0600

    Сделал правки второго ревью

--- scripts/card.js
+++ scripts/card.js
@@ -16,7 +16,10 @@ export class Card {
 
   generateCard() {
     this._element = this._getTemplate();
+
     this._cardImage = this._element.querySelector('.card__image');
+    this._trashButton = this._element.querySelector('.card__trash');
+    this._likeButton = this._element.querySelector('.card__like');
 
     this._element.querySelector('.card__text').textContent = this._title;      
     this._cardImage.src = this._link;
@@ -32,21 +35,22 @@ export class Card {
       this._handleOpenImage(this._title, this._link);
     })
 
-    this._trashButton = this._element.querySelector('.card__trash').addEventListener('click', () => {
+    this._trashButton.addEventListener('click', () => {
       this._removeCard();
     })
 
-    this._likeButton = this._element.querySelector('.card__like').addEventListener('click', (evt) => {
+    this._likeButton.addEventListener('click', (evt) => {
       this._toggleLikeButtonState(evt);
     })
   }
 
   _removeCard() {
     this._element.remove();
+    this._element = null;
   }
 
-  _toggleLikeButtonState(evt) {
-    evt.target.classList.toggle('card__like_active');
+  _toggleLikeButtonState() {
+    this._likeButton.classList.toggle('card__like_active');
   }
 
 }
